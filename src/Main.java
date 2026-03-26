1696967f719134662135d3cb4f2dfbe0123b5ad3
Lxyuck1025 <3228635325@qq.com>
2026-03-26 17:27:22 +0800

    3.26

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        // ERyder bike1 = new ERyder("12359i",89,true,5.452f);
-        // bike1.printBikeDetails();
-        // bike1.printRideDetails(40);
-        // ERyder bike2 = new ERyder("hxjsi",34,true,56.22f);
-        // bike2.ride();
-        // bike2.printBikeDetails();
-        // bike2.printRideDetails(54);
-        // System.out.println("can't use private calculateFare methods");
-        // bike2.publicCalculateFare(54);
-        // UserRegistration reg = new UserRegistration();
-        // reg.registration();
-        // System.out.println(reg.toString());
+        ERyder bike1 = new ERyder("12359i",89,true,5.452f);
+        bike1.printBikeDetails();
+        bike1.printRideDetails(40);
+        ERyder bike2 = new ERyder("hxjsi",34,true,56.22f);
+        bike2.ride();
+        bike2.printBikeDetails();
+        bike2.printRideDetails(54);
+        System.out.println("can't use private calculateFare methods");
+        bike2.publicCalculateFare(54);
+        UserRegistration reg = new UserRegistration();
+        reg.registration();
+        System.out.println(reg.toString());
         AdminPanel admin = new AdminPanel();
         admin.userManagementOptions();
     }
