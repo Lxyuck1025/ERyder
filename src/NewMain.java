f666f6a8fb6e7b57fd1aea79deaa9e9f08cb8462
Lxyuck1025 <3228635325@qq.com>
2026-03-26 17:24:08 +0800

    3.26

--- src/NewMain.java
+++
@@ -1,7 +0,0 @@
-public class NewMain {
-    public static void main(String[] args) {
-        UserRegistration reg = new UserRegistration();
-        reg.registration();
-        System.out.println(reg.toString());
-    }
-}
